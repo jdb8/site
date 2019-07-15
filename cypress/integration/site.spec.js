799c6bbaa9fe7b9b8ddc79034c131d9875ac9c9e
Joe Bateson <joe@joebateson.com>
2019-07-15 02:24:42 -0700

    Add basic cypress e2e tests (#52)
    Add basic cypress e2e tests with difficulty
    
    Also fix a11y violations and add azure pipelines to the repo because passing env variables in the web ui is too hard

---
+++ cypress/integration/site.spec.js
@@ -0,0 +1,12 @@
+describe('joebateson.com', function() {
+    it('passes smoketest', function() {
+        cy.visit('/');
+    });
+
+    it('passes a11y audits', () => {
+        cy.visit('/');
+        cy.injectAxe();
+        cy.wait(500);
+        cy.checkA11y();
+    });
+});
