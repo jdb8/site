ff2ccca4a318a757044256af0ed2b1569905136c
Joe Bateson <joe@joebateson.com>
2019-12-24 19:52:42 +0000

    Set up linting

--- cypress/integration/site.spec.js
+++ cypress/integration/site.spec.js
@@ -1,12 +1,14 @@
-describe('joebateson.com', function() {
-    it('passes smoketest', function() {
-        cy.visit('/');
-    });
+/* global cy */
 
-    it('passes a11y audits', () => {
-        cy.visit('/');
-        cy.injectAxe();
-        cy.wait(500);
-        cy.checkA11y();
-    });
+describe('joebateson.com', function() {
+  it('passes smoketest', function() {
+    cy.visit('/');
+  });
+
+  it('passes a11y audits', () => {
+    cy.visit('/');
+    cy.injectAxe();
+    cy.wait(500);
+    cy.checkA11y();
+  });
 });
