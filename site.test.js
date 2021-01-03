6d70106d422148e22be2fa08069b9f674b74dc7a
Joe Bateson <joe@joebateson.com>
2021-01-03 02:30:52 -0800

    cypress -> playwright

---
+++ site.test.js
@@ -0,0 +1,34 @@
+const { injectAxe, checkA11y } = require('axe-playwright');
+const { chromium } = require('playwright');
+const expect = require('expect');
+
+const BASE_URL = 'http://localhost:8080';
+
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await chromium.launch();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto(BASE_URL);
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+it('should work', async () => {
+  expect(await page.title()).toBe('Joe Bateson');
+});
+
+it('should pass a11y audit', async () => {
+  await injectAxe(page);
+  await checkA11y(page);
+});
