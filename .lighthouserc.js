2c6224a24980b9cb58efc1da52a54f21d046fb0b
Joe Bateson <joe@joebateson.com>
2021-09-05 19:26:01 -0700

    Turn off csp-xss audit

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,6 +2,9 @@ module.exports = {
   ci: {
     assert: {
       preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'off',
+      },
     },
     upload: {
       target: 'temporary-public-storage',
