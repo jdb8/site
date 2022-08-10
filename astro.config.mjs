b1245d64fb7fcf41a937f0f497b4e86f6abbc431
Joe Bateson <joe@joebateson.com>
2022-08-10 01:05:53 -0700

    Enable critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import astro from 'astro-critters';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joebateson.com',
+  integrations: [astro()],
 });
