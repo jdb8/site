c4fc991c6043801697356f6454fb3514ea74ba73
Joe Bateson <joe@joebateson.com>
2022-08-10 00:31:50 -0700

    Switch to astro

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './css/main.css';
-
-// If rIC isn't available, just immediately execute ¯\_(ツ)_/¯
-const init = window.requestIdleCallback || ((callbackFn) => callbackFn());
-init(() => document.body.focus());
