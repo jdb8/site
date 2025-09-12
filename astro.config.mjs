98b965d54732685d1937f11ba9327cee125da196
Joe Bateson <joe@joebateson.com>
2025-09-12 00:31:39 -0700

    Use React

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import astro from 'astro-critters';
 
 // https://astro.build/config
+import react from '@astrojs/react';
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://joebateson.com',
-  integrations: [astro()],
+  integrations: [astro(), react()],
 });
